c2bc5760775844086acf64a3ccebfe5387bcacf6
Teddy Ferdinan <64476430+teddy-f-47@users.noreply.github.com>
2022-04-06 21:31:31 +0200

    Added compiled APK
    Added compiled APK

---
+++ build/app/generated/source/buildConfig/release/pl/edu/pwr/lab2/i268893/task_manager/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package pl.edu.pwr.lab2.i268893.task_manager;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "pl.edu.pwr.lab2.i268893.task_manager";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
